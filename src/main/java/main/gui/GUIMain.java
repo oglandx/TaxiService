ba875db461c83083286af00dbc57c8e38cc88fc3
oglandx <kosolapov.semyon@gmail.com>
2016-06-13 14:22:24 +0000

    first steps in gui

---
+++ src/main/java/main/gui/GUIMain.java
@@ -0,0 +1,12 @@
+package main.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by oglandx on 6/13/16.
+ */
+public class GUIMain extends JFrame {
+    private JPanel panel;
+    private JButton signInButton;
+    private JButton signUpButton;
+}
