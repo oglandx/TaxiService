5128b87a3e8d10d81369bad2692b30e2894dbe53
oglandx <kosolapov.semyon@gmail.com>
2016-06-16 01:05:05 +0000

    i hate these settings

---
+++ src/test/java/main/repository/RepositoryTest.java
@@ -0,0 +1,74 @@
+package main.repository;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by oglandx on 6/16/16.
+ */
+public class RepositoryTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getDataMapper() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void all() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void get() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void get1() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void create() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void remove() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void remove1() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void filter() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void updateDb() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void closeDataMapperConnection() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void enableDataMapper() throws Exception {
+
+    }
+
+}
